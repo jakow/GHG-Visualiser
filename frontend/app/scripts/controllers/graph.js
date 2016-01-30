353837566cf52ba5df8729ba9e3d14e0c0aafa26
jakow <qbol1234@hotmail.com>
2016-01-30 22:07:34 +0000

    add map and graph controllers

---
+++ frontend/app/scripts/controllers/graph.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendApp.controller:GraphCtrl
+ * @description
+ * # GraphCtrl
+ * Controller of the frontendApp
+ */
+angular.module('frontendApp')
+  .controller('GraphCtrl', function () {
+
+
+
+  });
---
+++ frontend/app/scripts/controllers/map.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendApp.controller:MapcontrollerCtrl
+ * @description
+ * # MapcontrollerCtrl
+ * Controller of the frontendApp
+ */
+angular.module('frontendApp')
+  .controller('MapCtrl', function ($scope, $window) {
+   $scope.mapOptions =  {
+      lat: 51.505,
+      lon: -0.09,
+      zoom: 8
+    };
+
+    $scope.width = 800; //DOM Manipulation in controller :(
+    $scope.height = 300;
+
+  });
