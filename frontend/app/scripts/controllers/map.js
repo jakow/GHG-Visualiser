82ab373f616d1299dbf8b1006a8decdb27b8a6f7
wojdziw <wojciech.dziwulski@gmail.com>
2016-01-31 01:53:12 +0000

    Added map pointers

--- frontend/app/scripts/controllers/map.js
+++ frontend/app/scripts/controllers/map.js
@@ -17,5 +17,7 @@ angular.module('frontendApp')
 
     $scope.width = 500; //DOM Manipulation in controller :(
     $scope.height = 700;
-
+    $scope.markers = [{'name':'London', 'lat':51.505, 'lon':-0.09},
+      {'name':'Oxford', 'lat':51.751, 'lon':-1.255},
+      {'name':'Edinburgh', 'lat':55.953, 'lon':-3.188}];
   });
